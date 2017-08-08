b63b7c82d28c2aacd778414224658b44a5bb7722
panyujin <panyujinyx@gmail.com>
2017-08-08 22:41:06 +0800

    add bootstrap flash function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
   get "/jquery-3" => "pages#jquery_3"
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
 
-  root "pages#jquery_1"
+  resources :posts
+
+  root "posts#index"
 end
